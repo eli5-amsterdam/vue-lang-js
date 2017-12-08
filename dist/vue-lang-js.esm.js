6bfaf77fadef3f312d4506f487282e751222bd26
Andre de Waard <andre@eli5.io>
2017-12-08 16:27:10 +0100

    Changes methods to directives

--- dist/vue-lang-js.esm.js
+++ dist/vue-lang-js.esm.js
@@ -1,5 +1,5 @@
 /*!
- * vue-lang-js v0.1.0 
+ * vue-lang-js v1.0.0 
  * (c) 2017 undefined
  * Released under the MIT License.
  */
@@ -8,8 +8,8 @@ import Lang from 'lang.js';
 var plugin = {
 	install: function install(Vue, options) {
 		// Default options
-		var Locale = options.locale || "en";
-		var fallbackLocale = options.fallback || "en";
+		var Locale = options.locale || 'en';
+		var fallbackLocale = options.fallback || 'en';
 		var messages = options.messages || {};
 
 		var lang = new Lang({
@@ -18,29 +18,31 @@ var plugin = {
 			fallback: fallbackLocale
 		});
 
-		Object.defineProperty(Vue.prototype, "$lang", {
+		Object.defineProperty(Vue.prototype, '$lang', {
 			get: function() {
 				return this.$root._lang;
 			}
 		});
 
+		Vue.prototype.$trans = function(key, options) {
+			return lang.trans(key, options);
+		};
+
+		Vue.prototype.$t = function(key, options) {
+			return this.$trans(key, options);
+		};
+
+		Vue.prototype.$choice = function(key, plural, options) {
+			return lang.choice(key, plural, options);
+		};
+
+		Vue.prototype.$tc = function(key, plural, options) {
+			return this.$choice(key, plural, options);
+		};
+
 		Vue.mixin({
 			beforeCreate: function beforeCreate() {
-				Vue.util.defineReactive(this, "_lang", lang);
-			},
-			methods: {
-				trans: function trans(key, options) {
-					return lang.trans(key, options);
-				},
-				t: function t(key, options) {
-					return this.trans(key, options);
-				},
-				choice: function choice(key, plural, options) {
-					return lang.choice(key, plural, options);
-				},
-				tc: function tc(key, plural, options) {
-					return this.choice(key, plural, options);
-				}
+				Vue.util.defineReactive(this, '_lang', lang);
 			}
 		});
 	}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import Lang from "lang.js";
+import Lang from 'lang.js';
 
 const plugin = {
 	install(Vue, options) {
 		// Default options
-		const Locale = options.locale || "en";
-		const fallbackLocale = options.fallback || "en";
+		const Locale = options.locale || 'en';
+		const fallbackLocale = options.fallback || 'en';
 		const messages = options.messages || {};
 
 		const lang = new Lang({
@@ -13,29 +13,31 @@ const plugin = {
 			fallback: fallbackLocale
 		});
 
-		Object.defineProperty(Vue.prototype, "$lang", {
+		Object.defineProperty(Vue.prototype, '$lang', {
 			get: function() {
 				return this.$root._lang;
 			}
 		});
 
+		Vue.prototype.$trans = function(key, options) {
+			return lang.trans(key, options);
+		};
+
+		Vue.prototype.$t = function(key, options) {
+			return this.$trans(key, options);
+		};
+
+		Vue.prototype.$choice = function(key, plural, options) {
+			return lang.choice(key, plural, options);
+		};
+
+		Vue.prototype.$tc = function(key, plural, options) {
+			return this.$choice(key, plural, options);
+		};
+
 		Vue.mixin({
 			beforeCreate() {
-				Vue.util.defineReactive(this, "_lang", lang);
-			},
-			methods: {
-				trans(key, options) {
-					return lang.trans(key, options);
-				},
-				t(key, options) {
-					return this.trans(key, options);
-				},
-				choice(key, plural, options) {
-					return lang.choice(key, plural, options);
-				},
-				tc(key, plural, options) {
-					return this.choice(key, plural, options);
-				}
+				Vue.util.defineReactive(this, '_lang', lang);
 			}
 		});
 	}
